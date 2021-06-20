cb3b483b6ac14159fd9ea3827e7711f81967ac73
123armi <75014697+123armi@users.noreply.github.com>
2021-06-20 21:13:43 +0530

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -19,7 +19,7 @@ function send()
     number2 = document.getElementById("number2_input").value;
     actual_answer = parseInt(number1) * parseInt(number2);
 
-    question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
+    question_number = "<h4 style='color:black;'>" + number1 + " X " + number2 + "</h4>";
     input_box = "<br> Answer : <input type= 'text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>"
     row = question_number + input_box + check_button;
